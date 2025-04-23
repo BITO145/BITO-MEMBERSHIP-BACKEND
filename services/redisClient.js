a998f3bce652836b3f69ac739e88740642bee829
Bugbuster2004 <siddhant6338bansal@gmail.com>
2025-04-23 16:26:49 +0530

    added redis update

--- services/redisClient.js
+++ services/redisClient.js
@@ -2,10 +2,6 @@ import { createClient } from "redis";
 
 export const redisClient = createClient({
   url: process.env.REDIS_URL,
-  socket: {
-    tls: true,
-    rejectUnauthorized: false,
-  },
 });
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
