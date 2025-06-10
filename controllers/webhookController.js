63cf71d52950bf78a323037959f3ef04274b3105
Bugbuster2004 <siddhant6338bansal@gmail.com>
2025-06-10 21:34:25 +0530

    fixed bug

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -28,7 +28,6 @@ export const receiveEventWebhook = async (req, res) => {
     if (
       !hmrsEventId ||
       !eventName ||
-      !link ||
       !eventStartTime ||
       !eventEndTime ||
       !eventDate ||
